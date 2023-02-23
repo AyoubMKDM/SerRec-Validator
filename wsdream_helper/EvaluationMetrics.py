5c9781663182af95c5679cbcf769f4f54ef897f5
Ayoub Mokeddem <ayoubmkdm3@gmail.com>
2023-02-23 17:03:03 +0100

    Tweaking the evaluationMetrics module

--- wsdream_helper/EvaluationMetrics.py
+++ wsdream_helper/EvaluationMetrics.py
@@ -5,40 +5,62 @@ from surprise import accuracy
 from collections import defaultdict
 import itertools
 
-class Metrics:
+def MAE(predictions, verbose=True):
+    return accuracy.mae(predictions, verbose=verbose)
 
-    def MAE(predictions):
-        return accuracy.mae(predictions, verbose=False)
+def RMSE(predictions, verbose=True):
+    return accuracy.rmse(predictions, verbose=verbose)
 
-    def RMSE(predictions):
-        return accuracy.rmse(predictions, verbose=False)
+def get_top_n(predictions, n=10, minimumRating=0.0, verbose=False):
+    topN = defaultdict(list)
+    for userID, serviceID, actualRating, estimatedRating, _ in predictions:
+        if (estimatedRating >= minimumRating):
+            topN[int(userID)].append((int(serviceID), estimatedRating))
 
-    def get_top_n(predictions, n=10, minimumRating=0.0):
-        topN = defaultdict(list)
+    for userID, ratings in topN.items():
+        ratings.sort(key=lambda x: x[1], reverse=True)
+        topN[int(userID)] = ratings[:n]
+        if verbose:
+            print(f'Top {n} recommendations for user with id "{userID}" are:')
+            print('\t\t', *ratings[:n],sep="\n\t\t")
 
+    return topN
 
-        for userID, serviceID, actualRating, estimatedRating, _ in predictions:
-            if (estimatedRating >= minimumRating):
-                topN[int(userID)].append((int(serviceID), estimatedRating))
+def hit_rate(top_n_predicted, left_out_predictions, verbose=True):
+    hits = 0
+    total = 0
 
-        for userID, ratings in topN.items():
-            ratings.sort(key=lambda x: x[1], reverse=True)
-            topN[int(userID)] = ratings[:n]
+    # For each left-out rating
+    for left_out_item in left_out_predictions:
+        userID = left_out_item[0]
+        leftOutServiceID = left_out_item[1]
+        # Is it in the predicted top n for this user?
+        hit = False
+        for serviceID, predictedRating in top_n_predicted[int(userID)]:
+            if (int(leftOutServiceID) == int(serviceID)):
+                hit = True
+                break
+        if (hit) :
+            hits += 1
 
-        return topN
+        total += 1
+    if verbose:
+        print(f'Hit rate = \t {hits/total:.4f}')
+    # Compute overall precision
+    return hits/total
 
-    def hit_rate(top_n_predicted, left_out_predictions):
-        hits = 0
-        total = 0
+def cumulative_hit_rate(top_n_predicted, left_out_predictions, ratingCutoff=0, verbose=True):
+    hits = 0
+    total = 0
 
-        # For each left-out rating
-        for left_out_item in left_out_predictions:
-            userID = left_out_item[0]
-            leftOutServiceID = left_out_item[1]
+    # For each left-out rating
+    for userID, leftOutServiceID, actualRating, estimatedRating, _ in left_out_predictions:
+        # Only look at ability to recommend things the users actually liked...
+        if (actualRating >= ratingCutoff):
             # Is it in the predicted top 10 for this user?
             hit = False
             for serviceID, predictedRating in top_n_predicted[int(userID)]:
-                if (int(leftOutServiceID) == int(serviceID)):
+                if (int(leftOutServiceID) == serviceID):
                     hit = True
                     break
             if (hit) :
@@ -46,114 +68,103 @@ class Metrics:
 
             total += 1
 
-        # Compute overall precision
-        return hits/total
+    if verbose:
+        print(f'cHR = \t {hits/total:.4f}')
+    # Compute overall precision
+    return hits/total
 
-    def cumulative_hit_rate(top_n_predicted, left_out_predictions, ratingCutoff=0):
-        hits = 0
-        total = 0
+def rating_hit_rate(top_n_predicted, left_out_predictions, verbose=True):
+    hits = defaultdict(float)
+    total = defaultdict(float)
+    rHR_list = []
 
-        # For each left-out rating
-        for userID, leftOutServiceID, actualRating, estimatedRating, _ in left_out_predictions:
-            # Only look at ability to recommend things the users actually liked...
-            if (actualRating >= ratingCutoff):
-                # Is it in the predicted top 10 for this user?
-                hit = False
-                for serviceID, predictedRating in top_n_predicted[int(userID)]:
-                    if (int(leftOutServiceID) == serviceID):
-                        hit = True
-                        break
-                if (hit) :
-                    hits += 1
+    # For each left-out rating
+    for userID, leftOutServiceID, actualRating, estimatedRating, _ in left_out_predictions:
+        # Is it in the predicted top N for this user?
+        hit = False
+        for serviceID, predictedRating in top_n_predicted[int(userID)]:
+            if (int(leftOutServiceID) == serviceID):
+                hit = True
+                break
+        if (hit) :
+            hits[actualRating] += 1
+        total[actualRating] += 1
 
-                total += 1
+    # Compute overall precision
+    for rating in sorted(hits.keys()):
+        rHR_list.append((rating, hits[rating] / total[rating]))
+    if verbose:
+        print('rHr = ', *rHR_list, sep='\n\t')
+    return rHR_list
 
-        # Compute overall precision
-        return hits/total
+def average_reciprocal_hit_rank(top_n_predicted, left_out_predictions, verbose=True):
+    summation = 0
+    total = 0
+    # For each left-out rating
+    for userID, leftOutServiceID, actualRating, estimatedRating, _ in left_out_predictions:
+        # Is it in the predicted top N for this user?
+        hitRank = 0
+        rank = 0
+        for serviceID, predictedRating in top_n_predicted[int(userID)]:
+            rank = rank + 1
+            if (int(leftOutServiceID) == serviceID):
+                hitRank = rank
+                break
+        if (hitRank > 0) :
+            summation += 1.0 / hitRank
 
-    def rating_hit_rate(top_n_predicted, left_out_predictions):
-        hits = defaultdict(float)
-        total = defaultdict(float)
+        total += 1
+    if verbose:
+        print(f'ARHR = \t{summation / total:.4}')
+    return summation / total
 
-        # For each left-out rating
-        for userID, leftOutServiceID, actualRating, estimatedRating, _ in left_out_predictions:
-            # Is it in the predicted top N for this user?
-            hit = False
-            for serviceID, predictedRating in top_n_predicted[int(userID)]:
-                if (int(leftOutServiceID) == serviceID):
-                    hit = True
-                    break
-            if (hit) :
-                hits[actualRating] += 1
+# What percentage of users have at least one "good" recommendation
+def user_coverage(top_n_predicted, number_users, ratingThreshold=0, verbose=True):
+    hits = 0
+    for userID in top_n_predicted.keys():
+        hit = False
+        for serviceID, predictedRating in top_n_predicted[userID]:
+            if (predictedRating >= ratingThreshold):
+                hit = True
+                break
+        if (hit):
+            hits += 1
 
-            total[actualRating] += 1
+    if verbose:
+        print(f'Coverage = \t{hits/number_users:.4}')
+    return hits / number_users
 
-        # Compute overall precision
-        for rating in sorted(hits.keys()):
-            print (rating, hits[rating] / total[rating])
+def Diversity(top_n_predicted, sims_algo):
+    n = 0
+    total = 0
+    sims_matrix = sims_algo.compute_similarities()
+    for user_id in top_n_predicted.keys():
+        pairs = itertools.combinations(top_n_predicted[user_id], 2)
+        for pair in pairs:
+            service1 = pair[0][0]
+            service2 = pair[1][0]
+            innerID1 = sims_algo.trainset.to_inner_iid(str(service1))
+            innerID2 = sims_algo.trainset.to_inner_iid(str(service2))
+            similarity = sims_matrix[innerID1][innerID2]
+            total += similarity
+            n += 1
 
-    def average_reciprocal_hit_rank(top_n_predicted, left_out_predictions):
-        summation = 0
-        total = 0
-        # For each left-out rating
-        for userID, leftOutServiceID, actualRating, estimatedRating, _ in left_out_predictions:
-            # Is it in the predicted top N for this user?
-            hitRank = 0
-            rank = 0
-            for serviceID, predictedRating in top_n_predicted[int(userID)]:
-                rank = rank + 1
-                if (int(leftOutServiceID) == serviceID):
-                    hitRank = rank
-                    break
-            if (hitRank > 0) :
-                summation += 1.0 / hitRank
+    S = total / n
+    return (1-S)
 
-            total += 1
+def novelty(top_n_predicted, rankings, verbose=True):
+    n = 0
+    total = 0
+    for userID in top_n_predicted.keys():
+        for rating in top_n_predicted[userID]:
+            serviceID = rating[0]
+            rank = rankings[serviceID]
+            total += rank
+            n += 1
 
-        return summation / total
-
-    # What percentage of users have at least one "good" recommendation
-    def user_coverage(top_n_predicted, number_users, ratingThreshold=0):
-        hits = 0
-        for userID in top_n_predicted.keys():
-            hit = False
-            for serviceID, predictedRating in top_n_predicted[userID]:
-                if (predictedRating >= ratingThreshold):
-                    hit = True
-                    break
-            if (hit):
-                hits += 1
-
-        return hits / number_users
-
-    def Diversity(top_n_predicted, sims_algo):
-        n = 0
-        total = 0
-        sims_matrix = sims_algo.compute_similarities()
-        for user_id in top_n_predicted.keys():
-            pairs = itertools.combinations(top_n_predicted[user_id], 2)
-            for pair in pairs:
-                service1 = pair[0][0]
-                service2 = pair[1][0]
-                innerID1 = sims_algo.trainset.to_inner_iid(str(service1))
-                innerID2 = sims_algo.trainset.to_inner_iid(str(service2))
-                similarity = sims_matrix[innerID1][innerID2]
-                total += similarity
-                n += 1
-
-        S = total / n
-        return (1-S)
-
-    def novelty(top_n_predicted, rankings):
-        n = 0
-        total = 0
-        for userID in top_n_predicted.keys():
-            for rating in top_n_predicted[userID]:
-                serviceID = rating[0]
-                rank = rankings[serviceID]
-                total += rank
-                n += 1
-        return total / n
+    if verbose:
+        print(f'Novelty = \t{total/n:.4}')
+    return total / n
 
 
 
