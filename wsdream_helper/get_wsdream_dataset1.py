d556f64a0faf65d9224322bfa457f7b76a74070f
Ayoub Mokeddem <ayoubmkdm3@gmail.com>
2023-03-19 14:55:40 +0100

    fixing the get_wsdream_dataset1 bug

--- wsdream_helper/get_wsdream_dataset1.py
+++ wsdream_helper/get_wsdream_dataset1.py
@@ -1,12 +1,9 @@
-from .wsdream_utility import dataset_downloader
-from .get_input_args import get_input_args
+from . import wsdream_utility
+from . import get_input_args
 
-# TODO fix the bug 
-# from .wsdream_utility import dataset_downloader
-# ImportError: attempted relative import with no known parent package
 def main():
-    in_args = get_input_args()
-    dataset_downloader(dir=in_args.dir,url=in_args.url)
+    in_args = get_input_args.get_input_args()
+    wsdream_utility.dataset_downloader(dir=in_args.dir,url=in_args.url)
 
 if __name__ == "__main__":
     main()
