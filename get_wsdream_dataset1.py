1f8aa3f7dbb41546db0152a3ab51b81777a08e89
Ayoub Mokeddem <ayoubmkdm3@gmail.com>
2023-03-22 12:32:51 +0100

    updating get_wsdream_dataset module and rename it download_wsdream_dataset1

--- get_wsdream_dataset1.py
+++
@@ -1,9 +0,0 @@
-from wsdream_helper import wsdream_utility
-from wsdream_helper import get_input_args
-
-def main():
-    in_args = get_input_args.get_input_args()
-    wsdream_utility.dataset_downloader(dir=in_args.dir,url=in_args.url)
-
-if __name__ == "__main__":
-    main()
--- wsdream_helper/wsdream_utility.py
+++ wsdream_helper/wsdream_utility.py
@@ -1,69 +1,11 @@
-import urllib.request as urlopener
 import os
-from zipfile import ZipFile
-import shutil
 import pandas as pd
 import numpy as np
 from surprise import Dataset
 from surprise import Reader
 from .utility import DatasetFactory,NormalizationStrategy
 from .Normalization import NormalizationBasic
-
-
-
-def dataset_downloader(dir=None, url="https://zenodo.org/record/1133476/files/wsdream_dataset1.zip?download=1"):
-    """
-    Retrieve and download the WS-DREAM dataset as a zip file from the specified URL, 
-    then extract the content from the zip file and delete the latter. This function 
-    will add four files (rtMatrix.txt, tpMatrix.txt, userlist.txt, wslist.txt) to the 
-    specified directory or the current directory if no directory is specified.
-
-    Parameters:
-    dir (str): The directory where the dataset will be stored. Default is the current directory.
-    url (str): The URL of the downloadable zip file. The URL can be modified to download the 
-    WS-DREAM first dataset in case of a broken link or changing in the repository.
-    The default is 'https://zenodo.org/record/1133476/files/wsdream_dataset1.zip?download=1'.
-
-    Returns:
-    None
-
-    Example:
-    >>> dataset_downloader('my_data_folder')
-    """
-    # TODO make this return the file name
-    # TODO Handle http and url exceptions .. Use https://docs.python.org/3/library/urllib.error.html#urllib.error.URLError 
-    file_name = url.split('/')[-1]
-    page = urlopener.urlopen(url)
-    meta = page.info()
-    file_size = int(meta.get("Content-Length")[0])
-    print ("Downloading: %s (%s bytes)" % (file_name, file_size))
-    urlopener.urlretrieve(url, file_name)
-    print ('Unzip data files...')
-    with ZipFile(file_name, 'r') as archive:
-        for name in archive.namelist():
-            filename = os.path.basename(name)
-            # skip directories
-            if not filename:
-                continue
-            # copy file (taken from zipfile's extract)
-            print (filename)
-            source = archive.open(name)
-            if(dir is None):
-                target = open(filename, "wb")
-            else:
-                extraction_path = os.path.join(os.getcwd(),dir)
-                if not os.path.exists(extraction_path):
-                    print(f"Creating '{dir}/' folder ...")
-                    os.mkdir(dir)
-                target = open(os.path.join(extraction_path,filename),'wb')
-            with source, target:
-                shutil.copyfileobj(source, target)
-
-    os.remove(file_name)
-    print('==============================================')
-    print('Downloading data done!\n')
-    pass
-
+from .download_wsdream_dataset1 import dataset_downloader
 
 def dataframe_fromtxt(file):
     """
