4936021da393e56d0761a4e36a6d5ff5a0711d6c
Ayoub Mokeddem <ayoubmkdm3@gmail.com>
2023-03-23 14:31:32 +0100

    WsdreamReader: updating the docstrings

--- wsdream_helper/wsdream_utility.py
+++ wsdream_helper/wsdream_utility.py
@@ -5,59 +5,30 @@ from surprise import Dataset
 from surprise import Reader
 from .utility import DatasetFactory,NormalizationStrategy
 from .Normalization import NormalizationBasic
-from .download_wsdream_dataset1 import dataset_downloader
 import errno
-
-def dataframe_fromtxt(file):
-    """
-    Reads a .txt file containing a list of users or services and returns a pandas DataFrame object.
-
-    Parameters:
-        file (str): The name of the .txt file to be read.
-
-    Returns:
-        pd.DataFrame: A DataFrame containing the content of the .txt file.
-
-    Example:
-        >>> dataframe_fromtxt('userlist.txt')
-            User ID 	IP Address  	Country 	    IP No.  	AS  	                                Latitude	Longitude
-        0   0	        12.108.127.138	United States	208437130	AS7018 AT&T Services, Inc.	            38	        -97
-        1   1	        12.46.129.15	United States	204374287	AS7018 AT&T Services, Inc.	            38.0464	    -122.23
-        2   2	        122.1.115.91	Japan	        2046915419	AS4713 NTT Communications Corporation	35.685	    139.7514
-            ..  ..
-    """
-    data_file = open(file, 'r', encoding='utf-8', errors='replace')
-    indices = data_file.readline().strip().split('\t')
-    indices = [index.strip('[]') for index in indices] 
-    # Creating the DataFrame of users/services with the title line first
-    df = pd.DataFrame(columns=indices)
-    data_file.readline()
-    for line in data_file:
-        df.loc[len(df)] = line.strip().split('\t')
-    df.replace(to_replace="null",value=None,inplace=True)
-    data_file.close()
-    return df
-
 
 class WsdreamReader:
     """
-    The Wsdream class is a singleton class that contains an instance of the WS-DREAM dataset, 
-    with the different tables i.e. usersList, servicesList, responseTimeMatrix, throughputMatrix. 
-    This class is designed to simplify the interaction with the dataset.
+    Singleton class that reads the dataset with the different tables from the hard drive and makes 
+    them available as Python data structures that are easy to manipulate.
 
     Attributes:
-
     usersList (pandas.DataFrame): contains information on 339 service users, 
-    including User ID, IP Address, Country, Continent, AS, Latitude, Longitude, Region, City.
+        including User ID, IP Address, Country, Continent, AS, Latitude, Longitude, Region, City.
     servicesList (pandas.DataFrame): contains information on the 5,825 Web services, 
-    including Service ID, WSDL Address, Service Provider, IP Address, Country, Continent, AS, Latitude, Longitude, Region, City.
+        including Service ID, WSDL Address, Service Provider, IP Address, Country, Continent, AS, Latitude, Longitude, Region, City.
     responseTimeMatrix (numpy.ndarray): 339 x 5825 user-item matrix of response-time.
     throughputMatrix (numpy.ndarray): 339 x 5825 user-item matrix for throughput.
-    Methods:
 
+    Methods:
     save_lists_tocsv(): saves the usersList and servicesList DataFrames into a CSV file when needed.
+
+    Parameters:
+    dir : str
+        The directory where the dataset files are located. If not provided, the current working directory is used.
+
+    Raises: 
     """
-    # TODO create a final static url var interactions_full_df
     __USERS_LIST_FILE_NAME="userlist.txt"
     __SERVICES_LIST_FILE_NAME="wslist.txt"
     __RESPONSE_TIME_MATRIX_FILE_NAME = "rtMatrix.txt"
@@ -65,7 +36,6 @@ class WsdreamReader:
     __dir = None
     __instance = None
 
-    # TODO this verification is slow and computationaly expensive check it or add a way to skip later
     def __new__(cls, dir=None):
         if (cls.__instance is None):
             print('Creating the dataset object ...')
@@ -87,8 +57,8 @@ class WsdreamReader:
 
     @classmethod
     def _files_reader(cls):
-        cls.usersList = dataframe_fromtxt(file=os.path.join(cls.__dir, cls.__USERS_LIST_FILE_NAME))
-        cls.servicesList = dataframe_fromtxt(file=os.path.join(cls.__dir, cls.__SERVICES_LIST_FILE_NAME))
+        cls.usersList = cls.dataframe_fromtxt(file=os.path.join(cls.__dir, cls.__USERS_LIST_FILE_NAME))
+        cls.servicesList = cls.dataframe_fromtxt(file=os.path.join(cls.__dir, cls.__SERVICES_LIST_FILE_NAME))
         cls.responseTimeMatrix = np.loadtxt(os.path.join(cls.__dir, cls.__RESPONSE_TIME_MATRIX_FILE_NAME))
         cls.throughputMatrix = np.loadtxt(os.path.join(cls.__dir, cls.__THROUGHPUT_MATRIX_FILE_NAME))  
         cls.servicesList['IP No.'].replace("0",value=None,inplace=True) 
@@ -113,9 +83,6 @@ class WsdreamReader:
                 raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), os.path.join(full_path, cls.__THROUGHPUT_MATRIX_FILE_NAME))
             
 
-
-
-    # TODO add normalisation attribute
     def _df_from_matrix(self, matrix):
         # Converting matrix to list
         list_dataset = self._list_from_matrix(self, matrix)
@@ -137,7 +104,36 @@ class WsdreamReader:
         """
         self.usersList.to_csv("usersList.csv")
         self.servicesList.to_csv("servicesList.csv")
-        pass
+
+    def dataframe_fromtxt(file : str) -> pd.DataFrame:
+        """
+        Reads a .txt file containing a list of users or services and returns a pandas DataFrame object.
+
+        Parameters:
+            file (str): The name of the .txt file to be read.
+
+        Returns:
+            pd.DataFrame: A DataFrame containing the content of the .txt file.
+
+        Example:
+            >>> dataframe_fromtxt('userlist.txt')
+                User ID 	IP Address  	Country 	    IP No.  	AS  	                                Latitude	Longitude
+            0   0	        12.108.127.138	United States	208437130	AS7018 AT&T Services, Inc.	            38	        -97
+            1   1	        12.46.129.15	United States	204374287	AS7018 AT&T Services, Inc.	            38.0464	    -122.23
+            2   2	        122.1.115.91	Japan	        2046915419	AS4713 NTT Communications Corporation	35.685	    139.7514
+                ..  ..
+        """
+        with open(file, 'r', encoding='utf-8', errors='replace') as data_file:
+            indices = data_file.readline().strip().split('\t')
+            indices = [index.strip('[]') for index in indices] 
+            # Creating the DataFrame of users/services with the title line first
+            df = pd.DataFrame(columns=indices)
+            data_file.readline()
+            for line in data_file:
+                df.loc[len(df)] = line.strip().split('\t')
+            df.replace(to_replace="null",value=None,inplace=True)
+            
+        return df
 
 class WsdreamDataset(DatasetFactory):
     def __init__(self, wsdream, normalization_strategy: NormalizationStrategy = NormalizationBasic) -> None:
