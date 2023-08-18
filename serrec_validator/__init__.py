b9719595a47b9334ca400f4da54eb29a38f99413
Ayoub Mokeddem <ayoubmkdm3@gmail.com>
2023-08-18 20:56:45 +0100

    Update __init__.py file

--- serrec_validator/__init__.py
+++ serrec_validator/__init__.py
@@ -1,2 +1,3 @@
 from .WsdreamHelper import WsdreamReader, WsdreamDataset
 from .ModelEvaluator import evaluate, benchmark, compare, display_results
+from .utility import Sets, DataSplitter
--- test.py
+++ test.py
@@ -1,118 +1,119 @@
-from serrec_validator import WsdreamHelper
+import serrec_validator
+# from serrec_validator import WsdreamHelper
 from surprise import SVD, SVDpp, NMF, SlopeOne, NormalPredictor, KNNBaseline, KNNBasic, KNNWithMeans, KNNWithMeans, KNNWithZScore, BaselineOnly, CoClustering
 from serrec_validator import EvaluationMetrics
 from surprise import KNNBaseline
-from serrec_validator.utility import DataSplitter
-from serrec_validator import ModelEvaluator
-from serrec_validator.WsdreamHelper import WsdreamDataset
+# from serrec_validator.utility import DataSplitter
+# from serrec_validator import ModelEvaluator
+# from serrec_validator.WsdreamHelper import WsdreamDataset
 from serrec_validator.Normalization import *
 from serrec_validator import WsdreamDownloader
 
-dataset = wsdream_utility.Wsdream()
-WsdreamDownloader.download('work-dir')
+# dataset = WsdreamHelper()
+# WsdreamDownloader.download('work-dir')
 
-wsdream = WsdreamHelper.WsdreamReader('work-dir')
-wsdream.save_list_tocsv('users_df')
-wsdream.save_list_tocsv('services_df')
+# wsdream = WsdreamHelper.WsdreamReader('work-dir')
+# wsdream.save_list_tocsv('users_df')
+# wsdream.save_list_tocsv('services_df')
 
-print("Loading response time data ...")
-data = dataset.get_responseTime(density=5)
+# print("Loading response time data ...")
+# data = dataset.get_responseTime(density=5)
 
-splits = DataSplitter(dataset, 5, 6)
-
-
-dataset = WsdreamDataset(WsdreamHelper.WsdreamReader('work-dir'))
-splits = DataSplitter(dataset, 5, 6).response_time
+# splits = DataSplitter(dataset, 5, 6)
 
 
-print("\nComputing item similarities so we can measure diversity later...")
-# fullTrainSet = data.build_full_trainset()
-# fullTrainSet = splits.trainset_from_full_data['response_time']
-fullTrainSet = splits.response_time_splits.full_trainSet
-sim_options = {'name': 'pearson_baseline', 'user_based': False}
-simsAlgo = KNNBaseline(sim_options=sim_options)
-simsAlgo.fit(fullTrainSet)
+dataset = serrec_validator.WsdreamDataset(serrec_validator.WsdreamReader('work-dir'))
+splits = serrec_validator.DataSplitter(dataset, 5, 6).response_time
 
 
-print("\nBuilding recommendation model...")
-# trainSet, testSet = splits.splitset_for_accuracy['response_time']
-trainSet, testSet = splits.response_time_splits.accuracy_splits
+# print("\nComputing item similarities so we can measure diversity later...")
+# # fullTrainSet = data.build_full_trainset()
+# # fullTrainSet = splits.trainset_from_full_data['response_time']
+# fullTrainSet = splits.response_time_splits.full_trainSet
+# sim_options = {'name': 'pearson_baseline', 'user_based': False}
+# simsAlgo = KNNBaseline(sim_options=sim_options)
+# simsAlgo.fit(fullTrainSet)
 
-algo = SVD(random_state=10)
-algo.fit(trainSet)
 
-print("\nComputing recommendations...")
-predictions = algo.test(testSet)
+# print("\nBuilding recommendation model...")
+# # trainSet, testSet = splits.splitset_for_accuracy['response_time']
+# trainSet, testSet = splits.response_time_splits.accuracy_splits
 
-print("\nEvaluating accuracy of model...")
-print("RMSE: ", EvaluationMetrics.RMSE(predictions))
-print("MAE: ", EvaluationMetrics.MAE(predictions))
+# algo = SVD(random_state=10)
+# algo.fit(trainSet)
 
-print("\nEvaluating top-10 recommendations...")
+# print("\nComputing recommendations...")
+# predictions = algo.test(testSet)
 
-# trainSet, testSet = splits.splitset_for_hit_rate['response_time']
-trainSet, testSet = splits.response_time_splits.hit_splits
+# print("\nEvaluating accuracy of model...")
+# print("RMSE: ", EvaluationMetrics.RMSE(predictions))
+# print("MAE: ", EvaluationMetrics.MAE(predictions))
 
-# Train model without left-out ratings
-algo.fit(trainSet)
+# print("\nEvaluating top-10 recommendations...")
 
-# Predicts ratings for left-out ratings only
-print("Predict ratings for left-out set...")
-leftOutPredictions = algo.test(testSet)
+# # trainSet, testSet = splits.splitset_for_hit_rate['response_time']
+# trainSet, testSet = splits.response_time_splits.hit_splits
 
-# Build predictions for all ratings not in the training set
-print("Predict all missing ratings...")
-# allPredictions = algo.test(splits.anti_testset_for_hit_rate["response_time"])
-allPredictions = algo.test(splits.response_time_splits.anti_testSet_for_hits)
+# # Train model without left-out ratings
+# algo.fit(trainSet)
 
-# Compute top 10 recs for each user
-print("Compute top 10 recs per user...")
-topNPredicted = EvaluationMetrics.get_top_n(allPredictions, n=10)
+# # Predicts ratings for left-out ratings only
+# print("Predict ratings for left-out set...")
+# leftOutPredictions = algo.test(testSet)
 
-# See how often we recommended a movie the user actually rated
-print("\nHit Rate: ", EvaluationMetrics.hit_rate(topNPredicted, leftOutPredictions))
+# # Build predictions for all ratings not in the training set
+# print("Predict all missing ratings...")
+# # allPredictions = algo.test(splits.anti_testset_for_hit_rate["response_time"])
+# allPredictions = algo.test(splits.response_time_splits.anti_testSet_for_hits)
 
-# Break down hit rate by rating value
-print("\nrHR (Hit Rate by Rating value): ")
-EvaluationMetrics.rating_hit_rate(topNPredicted, leftOutPredictions)
+# # Compute top 10 recs for each user
+# print("Compute top 10 recs per user...")
+# topNPredicted = EvaluationMetrics.get_top_n(allPredictions, n=10)
 
-# See how often we recommended a movie the user actually liked
-print("\ncHR (Cumulative Hit Rate, rating >= 4): ", EvaluationMetrics.cumulative_hit_rate(topNPredicted, leftOutPredictions))
+# # See how often we recommended a movie the user actually rated
+# print("\nHit Rate: ", EvaluationMetrics.hit_rate(topNPredicted, leftOutPredictions))
 
-# Compute ARHR
-print("\nARHR (Average Reciprocal Hit Rank): ", EvaluationMetrics.average_reciprocal_hit_rank(topNPredicted, leftOutPredictions))
+# # Break down hit rate by rating value
+# print("\nrHR (Hit Rate by Rating value): ")
+# EvaluationMetrics.rating_hit_rate(topNPredicted, leftOutPredictions)
 
-print("\nComputing complete recommendations, no hold outs...")
-algo.fit(fullTrainSet)
-# bigTestSet = fullTrainSet.build_anti_testset()
-# allPredictions = algo.test(splits.anti_testset_from_full_data['response_time'])
-allPredictions = algo.test(splits.response_time_splits.anti_testset_from_full_data)
-topNPredicted = EvaluationMetrics.get_top_n(allPredictions, n=10)
+# # See how often we recommended a movie the user actually liked
+# print("\ncHR (Cumulative Hit Rate, rating >= 4): ", EvaluationMetrics.cumulative_hit_rate(topNPredicted, leftOutPredictions))
 
-# Print user coverage with a minimum predicted rating of 4.0:
-print("\nUser coverage: ", EvaluationMetrics.user_coverage(topNPredicted, fullTrainSet.n_users))
+# # Compute ARHR
+# print("\nARHR (Average Reciprocal Hit Rank): ", EvaluationMetrics.average_reciprocal_hit_rank(topNPredicted, leftOutPredictions))
 
-# # Measure diversity of recommendations:
-# print("\nDiversity: ", EvaluationMetrics.Diversity(topNPredicted, simsAlgo))
+# print("\nComputing complete recommendations, no hold outs...")
+# algo.fit(fullTrainSet)
+# # bigTestSet = fullTrainSet.build_anti_testset()
+# # allPredictions = algo.test(splits.anti_testset_from_full_data['response_time'])
+# allPredictions = algo.test(splits.response_time_splits.anti_testset_from_full_data)
+# topNPredicted = EvaluationMetrics.get_top_n(allPredictions, n=10)
 
-# Measure novelty (average popularity rank of recommendations):
-print("\nNovelty (average popularity rank): ", EvaluationMetrics.novelty(topNPredicted, data.df, Wsdream.WsdreamReader.usersList.shape[0]))
+# # Print user coverage with a minimum predicted rating of 4.0:
+# print("\nUser coverage: ", EvaluationMetrics.user_coverage(topNPredicted, fullTrainSet.n_users))
 
-# wsdream = wsdream_utility.Wsdream()
-splits = DataSplitter(dataset, density=5)
-algo = SVD(random_state=10)
+# # # Measure diversity of recommendations:
+# # print("\nDiversity: ", EvaluationMetrics.Diversity(topNPredicted, simsAlgo))
 
-print(ModelEvaluator.evaluate(algo, splits))
-ModelEvaluator.evaluate(algo, splits)
-algos = [SVD(), SVDpp(), SlopeOne(), NMF(), NormalPredictor(), KNNBaseline(), KNNBasic(), KNNWithMeans(), KNNWithZScore(), BaselineOnly(), CoClustering()]
+# # Measure novelty (average popularity rank of recommendations):
+# print("\nNovelty (average popularity rank): ", EvaluationMetrics.novelty(topNPredicted, data.df, Wsdream.WsdreamReader.usersList.shape[0]))
 
-# algos = {
-#     'SVD 30 epochs' : SVD(n_epochs=30,random_state=10),
-#     'SVD 10 epoch' : SVD(n_epochs=10),
-#     'SVD 20 epochs' : SVD(n_epochs=20)
-# }
+# # wsdream = wsdream_utility.Wsdream()
+# splits = DataSplitter(dataset, density=5)
+# algo = SVD(random_state=10)
 
-# ModelEvaluator.compare(algos, data=splits, verbose=True)
-results = ModelEvaluator.benchmark(algos=algos, dataset=dataset, verbose=True)
-# print(results)
+# print(ModelEvaluator.evaluate(algo, splits))
+# ModelEvaluator.evaluate(algo, splits)
+# algos = [SVD(), SVDpp(), SlopeOne(), NMF(), NormalPredictor(), KNNBaseline(), KNNBasic(), KNNWithMeans(), KNNWithZScore(), BaselineOnly(), CoClustering()]
+
+# # algos = {
+# #     'SVD 30 epochs' : SVD(n_epochs=30,random_state=10),
+# #     'SVD 10 epoch' : SVD(n_epochs=10),
+# #     'SVD 20 epochs' : SVD(n_epochs=20)
+# # }
+
+# # ModelEvaluator.compare(algos, data=splits, verbose=True)
+# results = ModelEvaluator.benchmark(algos=algos, dataset=dataset, verbose=True)
+# # print(results)
 
